2157ab585f607892d283db719b254ca4fdf65233
Matteo <matteo.dicarlo@fairconnect.it>
2021-02-05 13:29:24 +0100

    update rest test

--- restservice/src/main/java/it/restclienttemplate/MyController.java
+++ restservice/src/main/java/it/restclienttemplate/MyController.java
@@ -1,5 +1,6 @@
 package it.restclienttemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -12,58 +13,56 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @Slf4j
 public class MyController {
 
+    public static final ObjectMapper mapper = new ObjectMapper();
 
-    @GetMapping(value = "/stringvalue", produces = MediaType.TEXT_PLAIN_VALUE)
-    @ResponseBody
-    public String stringValue() {
-        return "Hello there!";
-    }
 
-    /**
-     * It returns a string value, no param
-     * @return
-     */
-    @PostMapping(value = "/jsonvaluenoparampost")
+
+    @PostMapping(value = "/noparampost")
     @ResponseBody
-    public MyResponse jsonvaluenoparampost() {
+    public MyResponse jsonValueNoParamPost() {
         MyResponse response = new MyResponse();
-        response.setResponseValue("123");
+        response.setResponseValue("json value without param post");
         return response;
 
     }
 
-    /**
-     * It returns a string value, no param
-     * @return
-     */
-    @GetMapping(value = "/jsonvaluenoparamget")
+    @GetMapping(value = "/noparamget")
     @ResponseBody
-    public MyResponse jsonvaluenoparamget() {
+    public MyResponse jsonValueNoParamGet() {
         MyResponse response = new MyResponse();
-        response.setResponseValue("123");
+        response.setResponseValue("json value without param get");
         return response;
 
     }
 
-    /**
-     * It returns a json value, API called with param
-     * @return
-     */
-    @GetMapping(value = "/jsonvalue")
-    public ResponseEntity<MyResponse> jsonValue(@RequestBody MyRequest request) {
-        log.info("-- Received: " + request);
-        log.info("-- Received value: " + request.getRequestValue());
-        MyResponse response = new MyResponse();
-        response.setResponseValue("123");
+    @PostMapping(value = "/parampost")
+    public ResponseEntity<MyResponse> jsonValueWithParamPost(@RequestBody String requestAsString) throws IOException {
 
+        MyRequest receivedRequest = mapper.readValue(requestAsString,MyRequest.class);
+        log.info("-- Received request as a string: " + requestAsString);
+        log.info("-- Received request value: " + receivedRequest.getRequestValue());
+
+        MyResponse response = new MyResponse();
+        response.setResponseValue("json value with param post");
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
+    @GetMapping(value = "/paramget")
+    public ResponseEntity<MyResponse> jsonValueWithParamGet(@RequestBody String requestAsString) throws IOException {
 
+        MyRequest receivedRequest = mapper.readValue(requestAsString,MyRequest.class);
+        log.info("-- Received request as a string: " + requestAsString);
+        log.info("-- Received request value: " + receivedRequest.getRequestValue());
+
+        MyResponse response = new MyResponse();
+        response.setResponseValue("json value with param get");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
--- restservice/src/test/java/it/restclienttemplate/test/MyControllerTest.java
+++ restservice/src/test/java/it/restclienttemplate/test/MyControllerTest.java
@@ -2,12 +2,16 @@ package it.restclienttemplate.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.restclienttemplate.MyController;
+import it.restclienttemplate.restservice.MyRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,21 +33,52 @@ public class MyControllerTest {
         this.mockMvc = MockMvcBuilders.standaloneSetup(new MyController()).build();
     }
 
-    @Test
-    public void jsonValueNoParamGet() throws Exception {
-        this.mockMvc.perform(get("/jsonvaluenoparamget"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
+    public static final ObjectMapper mapper = new ObjectMapper();
 
-    @Test
-    public void jsonValueNoParamPost() throws Exception {
-        this.mockMvc.perform(post("/jsonvaluenoparampost"))
+
+        @Test
+    public void jsonValueNoParamPostTest() throws Exception {
+        this.mockMvc.perform(post("/noparampost"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"responseValue\":\"123\"}"));
+                .andExpect(content().json("{\"responseValue\":\"json value without param post\"}"));
     }
 
+        @Test
+    public void jsonValueNoParamGetTest() throws Exception {
+        this.mockMvc.perform(get("/noparamget"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"responseValue\":\"json value without param get\"}"));
+    }
+
+    @Test
+    public void jsonValueWithParamPostTest() throws Exception {
+        MyRequest req = new MyRequest();
+        req.setRequestValue("call value for jsonValueWithParamPost");
+
+        String jsonRequest = mapper.writeValueAsString(req);
+        this.mockMvc.perform(post("/parampost")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andDo(print())
+                .andExpect(content().json("{\"responseValue\":\"json value with param post\"}"))
+                .andReturn();
+    }
+
+    @Test
+    public void jsonValueWithParamGetTest() throws Exception {
+        MyRequest req = new MyRequest();
+        req.setRequestValue("call value for jsonValueWithParamGet");
+
+        String jsonRequest = mapper.writeValueAsString(req);
+        this.mockMvc.perform(get("/paramget")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andDo(print())
+                .andExpect(content().json("{\"responseValue\":\"json value with param get\"}"))
+                .andReturn();
+    }
 
 }
 
